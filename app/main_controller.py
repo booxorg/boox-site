045c898228789891b96f4d9fdfbf872f3f18ccc0
Denis ROZIMOVSCHII <rozimovschii.denis@gmail.com>
2018-06-04 23:44:17 +0300

    Added simplified routing, request object, changed architecture

--- app/main_controller.py
+++ app/main_controller.py
@@ -1,14 +1,36 @@
 # Main controller, should handle requests to the main page
+import liteframework.routing as Routing
+import liteframework.controller as Controller
 
-from liteframework.routing import Route
-from liteframework.controller import Controller
+@Routing.Route(url='/', method='GET')
+def index(request={}, variables={}):
+    pass_variables = {
+        'title' : 'BooX: the sharing platform'
+    }
+    return Controller.view('main.html', pass_variables)
 
-@Route(url='/{username::[A-Za-z]*}', method='GET')
-def index(variables):
-    pass_variables = dict()
-    if variables['username']:
-        pass_variables['username'] = variables['username']
-    else:
-        pass_variables['username'] = 'guest'
-    return Controller.view('main', pass_variables)
+@Routing.Route(url='/register', method='GET')
+def register(request={}, variables={}):
+    pass_variables = {
+        'title' : 'Register'
+    }
+    return Controller.view('register.html', pass_variables)
 
+@Routing.Route(url='/login', method='GET')
+def login(request={}, variables={}):
+    pass_variables = {
+        'title' : 'Login'
+    }
+    return Controller.view('login.html', pass_variables)    
+
+@Routing.Route(url='/add', method='GET')
+def add_book(request={}, variables={}):
+    pass_variables = {
+        'title' : 'Add a book'
+    }
+    return Controller.view('addbook.html', pass_variables)    
+
+@Routing.Route(url='/post', method='POST')
+def info(request={}, variables={}):
+    text = request.input.getfirst('test', 'no test')
+    return Controller.response(text)
