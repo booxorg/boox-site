c56db2e844cfea7c0a390e76f148c33982fe62c5
Denis ROZIMOVSCHII <rozimovschii.denis@gmail.com>
2018-06-11 23:14:55 +0300

    Added search ajax request

--- app/controllers/main_controller.py
+++ app/controllers/main_controller.py
@@ -1,6 +1,13 @@
 # Main controller, should handle requests to the main page
 import liteframework.routing as Routing
 import liteframework.controller as Controller
+import liteframework.global_function as Global 
+import liteframework.application as App
+
+@Global.GlobalFunction
+def api_address():
+    return App.config.get('API', 'api_address')
+
 
 @Routing.Route(url='/', method='GET')
 def index(request={}, variables={}):
@@ -29,3 +36,10 @@ def add_book(request={}, variables={}):
         'title' : 'Add a book'
     }
     return Controller.response_view('addbook.html', pass_variables)    
+
+@Routing.Route(url='/search', method='GET')
+def add_book(request={}, variables={}):
+    pass_variables = {
+        'title' : 'Search for a book'
+    }
+    return Controller.response_view('personalized_search.html', pass_variables)    
